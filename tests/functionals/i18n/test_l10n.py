0d40de8adf08208b925999463ff22cb83a9c28cb
Guillaume Gauvrit <guillaume@gauvr.it>
2025-12-21 16:00:39 +0100

    Update tests

--- tests/functionals/i18n/test_l10n.py
+++ tests/functionals/i18n/test_l10n.py
@@ -15,44 +15,44 @@ def globals():
     return lczr.as_dict()
 
 
-@x_component()
+@x_component(namespace="lib")
 def Gettext():
     return """<>{globals.gettext('The lazy dog')}</>"""
 
 
-@x_component()
+@x_component(namespace="lib")
 def Dgettext():
     return """<>{globals.dgettext('mydomain', 'The lazy dog')}</>"""
 
 
-@x_component()
+@x_component(namespace="lib")
 def Ngettext():
     return """<>{globals.ngettext('The lazy dog', 'The lazy dogs', 1)}</>"""
 
 
-@x_component()
+@x_component(namespace="lib")
 def Dngettext():
     return """
         <>{globals.dngettext('mydomain', 'The lazy dog', 'The lazy dogs', 1)}</>
         """
 
 
-@x_component()
+@x_component(namespace="lib")
 def Pgettext():
     return """<>{globals.pgettext('animal', 'The lazy dog')}</>"""
 
 
-@x_component()
+@x_component(namespace="lib")
 def Dpgettext():
     return """<>{globals.dpgettext('mydomain', 'animal', 'The lazy dog')}</>"""
 
 
-@x_component()
+@x_component(namespace="lib")
 def Npgettext():
     return """<>{globals.npgettext('animal', 'The lazy dog', 'The lazy dogs', 1)}</>"""
 
 
-@x_component()
+@x_component(namespace="lib")
 def Dnpgettext():
     return """
         <>
@@ -68,11 +68,39 @@ def Dnpgettext():
         """
 
 
+@x_component(namespace="app")
+def App():
+    return """
+    <>
+        <lib.Gettext/>
+    </>
+    """
+
+
+@x_component(namespace="app")
+def AppAttribute():
+    return """
+    <>
+        <div aria-label={globals.pgettext("aria-label", "The lazy dog")}/>
+    </>
+    """
+
+
 @pytest.fixture
-def catalog():
+def catalogs() -> dict[str, Catalog]:
     config = Configurator(Settings())
     config.include(".")
-    return config.build_catalog()
+    return config.build_catalogs()
+
+
+@pytest.fixture
+def catalog(catalogs: dict[str, Catalog]) -> Catalog:
+    return catalogs["lib"]
+
+
+@pytest.fixture
+def ns_catalog(catalogs: dict[str, Catalog]) -> Catalog:
+    return catalogs["app"]
 
 
 @pytest.mark.parametrize(
@@ -90,3 +118,30 @@ def catalog():
 )
 def test_localize(catalog: Catalog, msg: str, globals: dict[str, Callable[..., str]]):
     assert catalog.render(msg, globals=globals) == "Le chien fénéant"
+
+
+@pytest.mark.parametrize(
+    "msg",
+    [
+        pytest.param("<App/>", id="gettext"),
+    ],
+)
+def test_ns_localize(
+    ns_catalog: Catalog, msg: str, globals: dict[str, Callable[..., str]]
+):
+    assert ns_catalog.render(msg, globals=globals) == "Le chien fénéant"
+
+
+@pytest.mark.parametrize(
+    "msg",
+    [
+        pytest.param("<AppAttribute/>", id="pgettext"),
+    ],
+)
+def test_localize_attrs(
+    ns_catalog: Catalog, msg: str, globals: dict[str, Callable[..., str]]
+):
+    assert (
+        ns_catalog.render(msg, globals=globals)
+        == '<div aria-label="Le chien fénéant"/>'
+    )
